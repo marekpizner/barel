39e1963bed1b0de42850da7b12b6dae584d0446b
marekpizner <m.pizner@gmail.com>
2020-10-16 23:44:05 +0200

    adding data parser version 1

--- withoutrealestate/withoutrealestate/spiders/data_spider.py
+++ withoutrealestate/withoutrealestate/spiders/data_spider.py
@@ -5,7 +5,7 @@ class DataSpider(scrapy.Spider):
     name = "data"
 
     def start_requests(self):
-        for ids in range(0, 13):
+        for ids in range(0, 5):
             url = f'https://www.bezrealitky.sk/vypis/ponuka-prenajom/byt/bratislavsky-kraj?page={ids}'
             yield scrapy.Request(url=url, callback=self.parse)
 
@@ -16,3 +16,13 @@ class DataSpider(scrapy.Spider):
             link = sel.xpath('./@href').extract()
             link_text = sel.xpath('./text()').extract()
             print(link, link_text)
+            url = "https://www.bezrealitky.sk" + \
+                link[0]
+            print('Url: ', url)
+            return scrapy.Request(url, callback=self.parse_concrete_inzerat)
+
+    def parse_concrete_inzerat(self, response):
+        name = response.xpath(
+            '//span/text()').get()
+
+        print('Name: ', name)
