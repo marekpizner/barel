d9954adf0fb4088dd1675b6d4c2dc555fbdd3f0f
marekpizner <m.pizner@gmail.com>
2020-10-25 22:59:49 +0100

    add basic pagination

--- barel/map_site/urls.py
+++ barel/map_site/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import index
+from .views import GenericView
 
 urlpatterns = [
-    path('', index, name='home'),
+    # path('', index, name='home'),
+    path('all/', GenericView.as_view(), name='all-list'),
 ]
--- barel/map_site/views.py
+++ barel/map_site/views.py
@@ -2,14 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Advertise
 from django.template import loader
+from django.views.generic import ListView
 
 # Create your views here.
 
 
-def index(request):
-    all_advertises = Advertise.objects.all()
-    template = loader.get_template('map_site/index.html')
-    context = {
-        'all_advertises': all_advertises,
-    }
-    return HttpResponse(template.render(context, request))
+class GenericView(ListView):
+    model = Advertise
+    paginate_by = 2
+    # Default: <app_label>/<model_name>_list.html
+    template_name = 'map_site/index.html'
+    context_object_name = 'advertises'  # Default: object_list
+    queryset = Advertise.objects.all()
+
+
+# def index(request):
+#     all_advertises = Advertise.objects.all()
+#     template = loader.get_template('map_site/index.html')
+#     context = {
+#         'all_advertises': all_advertises,
+#     }
+#     return HttpResponse(template.render(context, request))
