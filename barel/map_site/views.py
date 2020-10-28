4cf50f63cbd4cb8e766c6142c46045295551c330
marekpizner <m.pizner@gmail.com>
2020-10-28 22:54:52 +0100

    add mapbox map

--- barel/map_site/views.py
+++ barel/map_site/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import Advertise
 from django.template import loader
 from django.views.generic import ListView
-
+import os
 # Create your views here.
 
 
@@ -15,11 +15,7 @@ class GenericView(ListView):
     context_object_name = 'advertises'  # Default: object_list
     queryset = Advertise.objects.all()
 
-
-# def index(request):
-#     all_advertises = Advertise.objects.all()
-#     template = loader.get_template('map_site/index.html')
-#     context = {
-#         'all_advertises': all_advertises,
-#     }
-#     return HttpResponse(template.render(context, request))
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['mapbox_key'] = os.environ['MAPBOX_KEY']
+        return context
