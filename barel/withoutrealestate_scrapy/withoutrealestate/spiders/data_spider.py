65cf32133d69a7d7104e20a8f69ddc8ccc22aee0
marekpizner <m.pizner@gmail.com>
2020-10-29 20:06:56 +0100

    add scraping lat, lng

--- barel/withoutrealestate_scrapy/withoutrealestate/spiders/data_spider.py
+++ barel/withoutrealestate_scrapy/withoutrealestate/spiders/data_spider.py
@@ -10,7 +10,7 @@ class DataSpider(scrapy.Spider):
     name = "data"
 
     def start_requests(self):
-        for ids in range(0, 2):
+        for ids in range(0, 10):
             url = f'https://www.bezrealitky.sk/vypis/ponuka-prenajom/byt/bratislavsky-kraj?page={ids}'
             yield scrapy.Request(url=url, callback=self.parse)
 
